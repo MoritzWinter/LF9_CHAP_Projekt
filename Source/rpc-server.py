7e235df7b66126eb71b5ff5da7df5c3402a9c91e
Moritz Winter <wintermoritz92@gmail.com>
2020-08-31 11:57:10 +0200

    Added random and hashlib function to the server

--- Source/rpc-server.py
+++ Source/rpc-server.py
@@ -2,6 +2,8 @@
 
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import random
+import hashlib
 
 
 # Restrict to a particular path.
@@ -13,22 +15,41 @@ server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=RequestHandler)
 server.register_introspection_functions()
 
-# Register pow() function; this will use the value of
+#randomNumber creates a random number between 1 and 9
+def randomNumber():
+    return random.randint(1,9)
+
+#Register randomNumber;
+server.register_function((randomNumber), 'randomNumber')
+
+
+#Inizialize passwort
+password = "1234"
+
+#Hash passwort and randomNumber
+hashedPasswordAndRandomNumber = hashlib.sha256()
+hashedPasswordAndRandomNumber.update(password.encode('utf-8'))
+hashedPasswordAndRandomNumber.update(str(randomNumber).encode('utf-8'))
+print(hashedPasswordAndRandomNumber.hexdigest())
+
+
+
+## Register pow() function; this will use the value of
 # pow.__name__ as the name, which is just 'pow'.
-server.register_function(pow)
+# server.register_function(pow)
 
-# Register a function under a different name
-def adder_function(x,y):
-    return x + y
-server.register_function(adder_function, 'add')
+## Register a function under a different name
+# def adder_function(x,y):
+#     return x + y
+# server.register_function(adder_function, 'add')
 
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods (in this case, just 'div').
-class MyFuncs:
-    def div(self, x, y):
-        return x // y
+## Register an instance; all the methods of the instance are
+## published as XML-RPC methods (in this case, just 'div').
+# class MyFuncs:
+#    def div(self, x, y):
+#        return x // y
 
-server.register_instance(MyFuncs())
+# server.register_instance(MyFuncs())
 
 # Run the server's main loop
 server.serve_forever()
