8b8610b8dad74a274c103e4850122696a2458c5e
Moritz Winter <wintermoritz92@gmail.com>
2020-08-31 11:58:16 +0200

    edited rpc-client added hashlib and calls the random function from the server

--- Source/rpc-client.py
+++ Source/rpc-client.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 import xmlrpc.client
+import hashlib
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-print(s.pow(2,3))  # Returns 2**3 = 8
-print(s.add(2,3))  # Returns 5
-print(s.div(5,2))  # Returns 5//2 = 2
+print(s.randomNumber()) # Returns a Random Number between 1 and 9
+print("Bitte geben Sie ihr Passwort ein: ")
+password = input()
 
-# Print list of available methods
-print(s.system.listMethods())
+# Hash Password and Random Number
+hashedPasswordAndRandomNumber = hashlib.sha256()
+hashedPasswordAndRandomNumber.update(password.encode('utf-8'))
+hashedPasswordAndRandomNumber.update(str(s.randomNumber).encode('utf-8'))
+print(hashedPasswordAndRandomNumber.hexdigest())
+
+## Print list of available methods
+# print(s.system.listMethods())
